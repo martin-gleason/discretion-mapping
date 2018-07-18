e318a087bf5f5b9d59d9c395c2c1698df8609c33
Martin Gleason <martin.gleason@cookcountycourt.org>
2018-07-18 12:59:14 -0500

    "third draft"

--- diversion_data.R
+++ diversion_data.R
@@ -7,24 +7,33 @@ diversion <- read_csv(file)
 
 diversion$ZIP <- as.factor(diversion$ZIP)
 
-diversion %>% 
+srz_graph <- diversion %>% 
   group_by(SEX, RACE, ZIP) %>% 
   ggplot(aes(x = ZIP, fill = RACE)) + 
   geom_histogram(stat = "count") +
-  facet_grid(~ SEX)
+  facet_grid(~ SEX) + 
+  theme(axis.text.x=element_blank())
 
 
-diversion %>% 
-  group_by(SEX, RACE, ZIP) %>% 
+srz_diversion <- diversion %>% 
+  group_by(RACE) %>% 
   summarize(total = n(),
             min = min(total),
             max = max(total),
             mean = mean(total),
-            median = median(total))
+            median = median(total)) %>%
+  filter(total > max)
+
 
 diversion %>% 
   group_by(ZIP, total_days)
 
 
-diversion %>% filter(total_days == 0)
-
+diversion %>% 
+  filter(total_days == 0) %>%
+  group_by(RACE) %>% 
+  ungroup() %>%
+  ggplot(aes(x = ZIP, fill = RACE)) + 
+  geom_histogram(stat = "count") +
+  facet_grid(~ SEX) +
+  coord_flip()
